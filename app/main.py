fb5f8d197e66ae5a006457953e193a47549d75e4
2020-4-04-Bor <2020-4-04-Bor>
2024-02-15 18:18:21 +0300

    refactor main and add all .idea files in .gitignore

--- app/main.py
+++ app/main.py
@@ -1,46 +1,136 @@
-import time
-import random
+from random import choice, shuffle, randint
+from time import time
 
-def generate_simple_rules(code_max, n_max, n_generate, log_oper_choice =["and"," or","not"]):
-	rules = []
-	for j in range (0, n_generate):
-		log_oper = random.choice(log_oper_choice) #not means and-not (neither)
-		if n_max < 2:
-			n_max = 2
-		n_items = random.randint(2, n_max)
-		items = []
-		for i in range(0, n_items):
-			items.append(random.randint(1, code_max))
-		rule = {
-			'if': {log_oper: items},
-			'then ':code_max+j
-		}
-		rules.append(rule)
-	random.shuffle(rules)
-	return (rules)
 
-def generate_seq_facts(M) :
-	facts = list(range (0,M))
-	random.shuffle(facts)
-	return facts
+def generate_simple_rules(code_max, n_max, n_generate, log_oper_choice=None):
+    if log_oper_choice is None:
+        log_oper_choice = ["and", "or", "not"]
+    rules = []
+    if n_max < 2:
+        n_max = 2
+    for j in range(0, n_generate):
+        log_oper = choice(log_oper_choice)  # not means and-not (neither)
+        n_items = randint(2, n_max)
+        items = []
+        for i in range(0, n_items):
+            items.append(randint(1, code_max))
+        rule = {
+            'if': {
+                log_oper: items
+            },
+            'then': code_max + j
+        }
+        rules.append(rule)
+    shuffle(rules)
+    return rules
+
+
+def generate_stairway_rules(code_max, n_max, n_generate, log_oper_choice=None):
+    if log_oper_choice is None:
+        log_oper_choice = ["and", "or", "not"]
+    rules = []
+    if n_max < 2:
+        n_max = 2
+    for j in range(0, n_generate):
+        log_oper = choice(log_oper_choice)  # not means and-not (neither)
+        n_items = randint(2, n_max)
+        items = []
+        for i in range(0, n_items):
+            items.append(i + j)
+        rule = {
+            'if': {
+                log_oper: items
+            },
+            'then': i + j + 1
+        }
+        rules.append(rule)
+    shuffle(rules)
+    return rules
+
+
+def generate_ring_rules(code_max, n_max, n_generate, log_oper_choice=None):
+    if log_oper_choice is None:
+        log_oper_choice = ["and", "or", "not"]
+    rules = generate_stairway_rules(code_max, n_max, n_generate - 1, log_oper_choice)
+    log_oper = choice(log_oper_choice)  # not means and-not (neither)
+    if n_max < 2:
+        n_max = 2
+    n_items = randint(2, n_max)
+    items = []
+    for i in range(0, n_items):
+        items.append(code_max - i)
+    rule = {
+        'if': {
+            log_oper: items
+        },
+        'then': 0
+    }
+    rules.append(rule)
+    shuffle(rules)
+    return rules
+
+
+def generate_random_rules(code_max, n_max, n_generate, log_oper_choice=None):
+    if log_oper_choice is None:
+        log_oper_choice = ["and", "or", "not"]
+    rules = []
+    if n_max < 2:
+        n_max = 2
+    for j in range(0, n_generate):
+
+        log_oper = choice(log_oper_choice)  # not means and-not (neither)
+
+        n_items = randint(2, n_max)
+        items = []
+        for i in range(0, n_items):
+            items.append(randint(1, code_max))
+        rule = {
+            'if': {
+                log_oper: items
+            },
+            'then': randint(1, code_max)
+        }
+        rules.append(rule)
+    shuffle(rules)
+    return rules
+
+
+def generate_seq_facts(M):
+    facts = list(range(0, M))
+    shuffle(facts)
+    return facts
+
 
 def generate_rand_facts(code_max, M):
-	facts = []
-	for i in range(0,M):
-		facts.append(random.randint(0, code_max) )
-	return facts
+    facts = []
+    for i in range(0, M):
+        facts.append(randint(0, code_max))
+    return facts
 
-#generate rules and facts and check time
-time_start = time.time()
+
+# samples:
+print('simple_rules:', generate_simple_rules(100, 4, 10))
+print('random_rules:', generate_random_rules(100, 4, 10))
+print('stairway_rules:', generate_stairway_rules(100, 4, 10, ["or"]))
+print('ring_rules:', generate_ring_rules(100, 4, 10, ["or"]))
+
+# generate rules and facts and check time
+time_start = time()
 N = 100000
 M = 1000
 rules = generate_simple_rules(100, 4, N)
 facts = generate_rand_facts(100, M)
-print("%d rules generated in %f seconds" % (N, time.time()-time_start))
-#load and validate rules
+print("%d rules generated in %f seconds" % (N, time() - time_start))
+
+# load and validate rules
 # YOUR CODE HERE
+
 print(rules[:2])
-#check facts vs rules
-time_start = time.time()
+print(facts)
+
+# check facts vs rules
+time_start = time()
+
 # YOUR CODE HERE
-print("%d facts validated vs %d rules in %f seconds" % (M, N, time.time()-time_start))
+
+print("%d facts validated vs %d rules in %f seconds" % (M, N, time() - time_start))
