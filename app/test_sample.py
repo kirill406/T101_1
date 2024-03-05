ef3d1831d2694f16a61a4661c9c20363b774dd87
2020-4-04-Bor <2020-4-04-Bor>
2024-03-05 19:00:07 +0300

    add results in readme

--- app/test_sample.py
+++ app/test_sample.py
@@ -1,5 +1,121 @@
 import main
 
+
+class TestValidators:
+
+    def test_base(self):
+        rules_in = [
+            {'if': {'or': [93, 8, 57, 16]}, 'then': 101},
+            {'if': {'not': [21, 15]}, 'then': 107},
+            {'if': {'and': [100, 2]}, 'then': 102},
+        ]
+        rules_out = [
+            {'if': {'or': [93, 8, 57, 16]}, 'then': 101},
+            {'if': {'not': [21, 15]}, 'then': 107},
+            {'if': {'and': [100, 2]}, 'then': 102},
+        ]
+
+        validated_rules = main.validate_rules(rules_in)
+
+        assert rules_out == validated_rules
+
+
+    def test_not_contradiction(self):
+        rules_in = [
+            {'if': {'not': [1, 2, 3]}, 'then': 1},
+        ]
+        rules_out = []
+
+        validated_rules = main.validate_rules(rules_in)
+
+        assert rules_out == validated_rules
+
+
+    def test_not_contradiction1(self):
+        rules_in = [
+            {'if': {'not': [1, 5]}, 'then': 2},
+            {'if': {'not': [2, 6]}, 'then': 3},
+            {'if': {'not': [3, 7]}, 'then': 1},
+        ]
+        rules_out = []
+
+        validated_rules = main.validate_rules(rules_in)
+
+        assert rules_out == validated_rules
+
+
+    def test_not_contradiction2(self):
+        rules_in = [
+            {'if': {'not': [1, 5]}, 'then': 2},
+            {'if': {'not': [2, 6]}, 'then': 3},
+        ]
+        rules_out = []
+
+        validated_rules = main.validate_rules(rules_in)
+
+        assert rules_out == validated_rules
+
+
+    def test_not_contradiction3(self):
+        rules_in = [
+            {'if': {'not': [1, 5]}, 'then': 2},
+            {'if': {'not': [2, 6]}, 'then': 3},
+        ]
+        rules_out = []
+
+        validated_rules = main.validate_rules(rules_in)
+
+        assert rules_out == validated_rules
+
+
+    def test_not_contradiction4(self):
+        rules_in = [
+            {'if': {'or': [1, 5]}, 'then': 2},
+            {'if': {'not': [2, 6]}, 'then': 3},
+        ]
+        rules_out = []
+
+        validated_rules = main.validate_rules(rules_in)
+
+        assert rules_out == validated_rules
+
+
+    def test_and_contradiction(self):
+        rules_in = [
+            {'if': {'and': [1, 2, 3]}, 'then': 1},
+        ]
+        rules_out = []
+
+        validated_rules = main.validate_rules(rules_in)
+
+        assert rules_out == validated_rules
+
+
+    def test_not2(self):
+        rules_in = [
+            {'if': {'or': [2, 5]}, 'then': 1},
+            {'if': {'not': [1, 6]}, 'then': 2},
+        ]
+        rules_out = []
+
+        validated_rules = main.validate_rules(rules_in)
+
+        assert rules_out == validated_rules
+
+
+    def test_reverse_not2(self):
+        rules_in = [
+            {'if': {'not': [1, 6]}, 'then': 2},
+            {'if': {'or': [2, 5]}, 'then': 1},
+        ]
+        rules_out = []
+
+        validated_rules = main.validate_rules(rules_in)
+
+        assert rules_out == validated_rules
+
+
+
 class TestResolves:
 
     def test_1(self):
@@ -10,82 +126,11 @@ class TestResolves:
         ]
         facts = [93, 45, 44, 12]
         max_fact_value = 100
-        validated_rules1, rules_with_100 = main.validate_rules_simple(rules)
 
-        add_fact = main.resolve(facts, rules, max_fact_value, rules_with_100)
+        validated_rules = main.validate_rules(rules)
+
+        add_fact = main.resolve(facts, validated_rules, max_fact_value)
         facts.extend(add_fact)
 
         assert set(facts) == {93, 101, 45, 44, 12, 107}
 
-
-    def test_100(self):
-        rules = [
-            {'if': {'and': [100, 44]}, 'then': 102},
-            {'if': {'or': [93, 8, 57, 16]}, 'then': 101},
-            {'if': {'not': [21, 15]}, 'then': 100},
-        ]
-        facts = [93, 45, 44, 12]
-
-        facts.extend(main.resolve(facts, rules))
-
-        assert set(facts) == {12, 44, 45, 93, 100, 101, 102}
-
-
-    def test_not(self):
-        rules = [
-            {'if': {'or': [93, 8]}, 'then': 21},
-            {'if': {'not': [21, 15]}, 'then': 100},
-        ]
-        facts = [93, 60]
-
-        facts.extend(main.resolve(facts, rules))
-
-        assert set(facts) == {93, 60, 21, 100}
-
-
-    def test_not2(self):
-        rules = [
-            {'if': {'not': [1]}, 'then': 2},
-            {'if': {'or': [2]}, 'then': 1},
-        ]
-        facts = [93, 60]
-
-        facts.extend(main.resolve(facts, rules))
-
-        assert set(facts) == {93, 60}
-
-
-    def test_revers_not2(self):
-        rules = [
-            {'if': {'or': [2]}, 'then': 1},
-            {'if': {'not': [1]}, 'then': 2},
-        ]
-        facts = [93, 60]
-
-        facts.extend(main.resolve(facts, rules))
-
-        assert set(facts) == {93, 60}
-
-
-    def test_twice(self):
-        rules = [
-            {'if': {'and': [12, 44]}, 'then': 102},
-            {'if': {'or': [93, 8]}, 'then': 102},
-        ]
-        facts = [93, 45, 44, 12]
-
-        facts.extend(main.resolve(facts, rules))
-
-        assert set(facts) == {93, 45, 44, 12, 102}  # not {93, 45, 44, 12, 102, 102}
-
-
-def test_add_100_in_facts():
-    rules = [
-        {'if': {'or': [93, 8, 57, 16]}, 'then': 100},
-    ]
-    facts = [93, 45, 44, 12]
-    facts_set = set(facts)
-
-    facts_set = main.add_100_in_facts(facts_set, rules)
-
-    assert facts_set == {93, 45, 44, 12, 100}
--- readme.md
+++ readme.md
@@ -7,5 +7,11 @@ docker run -it -v "${PWD}/app":/code/app --rm --name my-running-app my-python-ap
 '''
 ### Результаты
 
+100000 rules generated in 0.184759 seconds
+
+100000 rules validated in 272.283578 seconds
+
+1000 facts validated vs 100000 rules in 0.027036 seconds
+
 ### Графики
 
